670c50c626265d4b8abc5a212a85b047f0be3577
Germain <germain.buch@gmail.com>
2022-06-30 16:20:45 +0200

    changement-couleur-cyan

--- git-conflicts-2/git-conflicts-2/ViewController.swift
+++ git-conflicts-2/git-conflicts-2/ViewController.swift
@@ -13,7 +13,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
-        self.view.backgroundColor = .orange
+        self.view.backgroundColor = .cyan
         
         
     }
