187229e952ccc14ab5e1c74006566127f99019fb
Germain <germain.buch@gmail.com>
2022-06-30 14:48:16 +0200

    initial commit

---
+++ git-conflicts/git-conflicts/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  git-conflicts
+//
+//  Created by Student10 on 30/06/2022.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
