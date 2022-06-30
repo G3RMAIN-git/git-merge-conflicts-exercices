42fe85a75c270814dc521994096bfba20f5281de
Germain <germain.buch@gmail.com>
2022-06-30 16:11:05 +0200

    changement background purple 3

--- git-conflicts-2/git-conflicts-2/ViewController.swift
+++ git-conflicts-2/git-conflicts-2/ViewController.swift
@@ -13,7 +13,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
-        self.view.backgroundColor = .green
+        self.view.backgroundColor = .purple
         
         
     }
